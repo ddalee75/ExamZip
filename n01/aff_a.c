04caccf141d7706e07211abb404bcd285d9021bc
ddalee75 <92326016+ddalee75@users.noreply.github.com>
2022-03-31 15:23:17 +0200

    Update aff_a.c

--- n01/aff_a.c
+++ n01/aff_a.c
@@ -24,8 +24,11 @@ int	main(int ac, char **av)
 		while (av[1][i])
 		{
 			if (av[1][i] == 'a')
-				write(1, "a", 1);
-			i++;
+           		{
+                		write(1, "a\n", 2);
+                		return (0);
+            		}
+            		i++;
 		}
 	}
 	write (1, "\n", 1);
